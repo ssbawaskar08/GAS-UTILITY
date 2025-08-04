3b2fcefffdfb7435718d58be65f31beae8951ce2
Shreyas Bawaskar <shreyasbawaskar0812@gmail.com>
2025-08-04 23:27:53 +0530

    Update views.py

--- gas_utility/views.py
+++ gas_utility/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import redirect
 
 def home(request):
     # Redirect to customers/submit-request/ as the default page
+    # hi this is for my commit history
     return redirect('customers:submit_request')
